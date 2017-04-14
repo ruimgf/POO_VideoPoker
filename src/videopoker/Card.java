1c35222cfc6bde554b258ab549d85e605ae11438
Alexandre <Alexandre@Suface-Candeias.lan>
2017-04-14 22:39:34 +0100

    equlas to the card class added and tested

--- src/videopoker/Card.java
+++ src/videopoker/Card.java
@@ -15,6 +15,31 @@ public class Card {
 		return "Card "+value+suit+"";
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((suit == null) ? 0 : suit.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Card other = (Card) obj;
+		if (suit != other.suit)
+			return false;
+		if (value != other.value)
+			return false;
+		return true;
+	}
+
 	public static void main(String[] args) {
 		for (Suit S : Suit.values()){
 			for (CardValue CV : CardValue.values()){
@@ -22,6 +47,15 @@ public class Card {
 				System.out.println(first);
 			}
 		}
+		
+		Card second = new Card( Suit.DIAMONS,CardValue.TWO );
+		Card first = new Card(Suit.DIAMONS,CardValue.TWO);
+		
+		if(first.equals(second)){
+			System.out.print("equals a funcionar ");
+			System.out.println(first);
+		}
+		
 		
 	}
 	
