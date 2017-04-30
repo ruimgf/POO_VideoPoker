50cb650c3d21c8da17ec592e46d1b5355407e920
candeiasalexandre <candeiasalexandre@gmail.com>
2017-04-30 22:43:11 +0100

    implmentation of the statistics - comments add in some classes

---
+++ src/doublebonus10_7/Statistics_Result.java
@@ -0,0 +1,18 @@
+package doublebonus10_7;
+
+public class Statistics_Result extends Result {
+	Statistics game_stats;
+	
+	Statistics_Result(Statistics game_stats){
+		
+		this.game_stats = game_stats;
+		
+	}
+
+	@Override
+	public String toString() {
+		return "" + game_stats;
+	}
+	
+	
+}
