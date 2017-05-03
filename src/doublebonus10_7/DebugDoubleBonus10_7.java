c55b43f828a720846565b043071f35abeaf30154
candeiasalexandre <candeiasalexandre@gmail.com>
2017-05-03 19:19:00 +0100

    change the state diagram of the game to support deal without bet after the first deal in the begin of the game

--- src/doublebonus10_7/DebugDoubleBonus10_7.java
+++ src/doublebonus10_7/DebugDoubleBonus10_7.java
@@ -14,7 +14,8 @@ public class DebugDoubleBonus10_7 extends DoubleBonus10_7 {
 	
 	public Result deal(){
 		
-		if(this.gamestate == 2){
+
+		if(this.gamestate == 1 || this.gamestate == 3 || this.gamestate == 4){
 			
 			/*pick 5 cards*/
 			Card[] aux = new Card[5];
@@ -24,8 +25,22 @@ public class DebugDoubleBonus10_7 extends DoubleBonus10_7 {
 			/*pass cards to the player hand*/
 			this.game_cards.newCards(aux);
 			
-			/*change the game state to the next state = 3*/
-			this.gamestate = 3;
+			/*if it is on the gamestate 1 it goes to gamestate 2 and the money draw as been already done by bet*/
+			if(this.gamestate == 1){
+				this.gamestate =2;
+			}else if(this.gamestate == 3){
+				/*if it is on the gamestate 3 then we have to do the money draw whit the last_bet there and move to state 2*/
+				this.credits = this.credits - this.lastbet;
+				/*update the statistics because the money draw*/
+				this.game_stats.updateActualCredit(this.credits);
+				this.gamestate = 2;
+			}else{
+				/*if it is oh the gamestate 4 the money draw as already done in bet and we go to gamestate  2*/
+				this.gamestate = 2;
+			}
+			
+			
+			
 			/*update the number of deals in the statisctics*/
 			this.game_stats.addDeal();
 			/*return result of the bet*/
@@ -34,6 +49,7 @@ public class DebugDoubleBonus10_7 extends DoubleBonus10_7 {
 			
 			
 		}
+		
 		return new Invalid_Result("d: illegal command",this.credits);
 	}
 
