288f557f804072dabc4ee35e430b5a98473cf958
Rui Figueiredo <ruimgf95@gmail.com>
2017-05-08 16:17:00 +0100

    correct generateJar

--- generateJar.sh
+++ generateJar.sh
@@ -3,7 +3,7 @@ cd src
 javac cards/*.java
 javac videopoker/*.java
 javac players/*.java
-jar cfm ../mygame.jar manif.txt cards videopoker players
+jar cfm ../mygame.jar manif.txt cards videopoker players images
 rm cards/*.class
 rm videopoker/*.class
 rm players/*.class
