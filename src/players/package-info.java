e93947a24bea616839460f68553ab67b5251c29a
pedromartinho <pedro.martinho.uniplaces@gmail.com>
2017-05-10 23:06:41 +0100

    final java doc

--- src/players/package-info.java
+++ src/players/package-info.java
@@ -1,6 +1,6 @@
 /**
  * This package provides different game modes to interact with the interface {@link videopoker.Videopoker}
- * If the INTERFASE is used, the user can use the following game modes: 
+ * If the videopoker interface is used, the user can use the following game modes: 
  * Debug, Interactive, Simulation and GUI
  * The expected commands are the: bet (b), deal (d), hold (h), advise (a), credit ($), statistics (s) and quit (q)  
  * @author Alexandre , Rui , Pedro
