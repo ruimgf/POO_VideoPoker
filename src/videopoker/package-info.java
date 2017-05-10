8eb3276c055e25ffa4572bab187371d50b15b8b2
Rui Figueiredo <ruimgf95@gmail.com>
2017-05-10 21:46:47 +0100

    solve bug

--- src/videopoker/package-info.java
+++ src/videopoker/package-info.java
@@ -1,5 +1,5 @@
 /**
  * This package provides a Interface for a Videopoker implementation and also for the variation of that Videopoker
- * it also gives the class's that the Videopooker should return in each play , and finaly the exeption of a invalid playç
+ * it also gives the class's that the Videopooker should return in each play , and finaly the exeption of a invalid play
  */
 package videopoker;
